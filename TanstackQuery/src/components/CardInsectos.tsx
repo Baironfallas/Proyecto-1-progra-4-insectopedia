6f69b8d7b6c52bb6c2e2ca0e43bdf7efc3237b12
bairon <fallasbaltodanobairon@gmail.com>
2025-04-22 15:16:15 -0600

    Agregar soporte para la gestión de insectos con React Query y componentes de tarjeta

---
+++ TanstackQuery/src/components/CardInsectos.tsx
@@ -0,0 +1,29 @@
+import { insecto } from "../types/insecto";
+
+type Props = {
+  data: insecto;
+};
+
+const CardInsectos = ({ data }: Props) => {
+  return (
+    <div className="card">
+      <img src={data.img} alt={data.nombre} className="card-img" />
+      <div className="card-content">
+        <h3 className="card-title">{data.nombre}</h3>
+        <em>Apejs mellefiror</em>
+        <p className="card-type">
+          <strong>Tipo:</strong> {data.tipo}
+        </p>
+        <p className="card-curiosity">
+          <strong>Curiosidad:</strong> {data.curiosidad}
+        </p>
+        <div className="card-buttons">
+          <button className="btn-edit">Editar</button>
+          <button className="btn-delete">Eliminar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardInsectos;
