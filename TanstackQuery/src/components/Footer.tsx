a26ef176b6efc644b0101b93c92fde524aab092f
Jhonn Rojas <jhonngranadosrojas@email.com>
2025-04-24 00:03:29 -0600

    Footer realizado

---
+++ TanstackQuery/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { GiButterfly } from "react-icons/gi";
+
+const Footer =()=>{
+    return(
+        <footer className="footer">
+            <p className="footer-rights"><GiButterfly />© Insectopedia 2025. Todos los derechos reservados.</p>
+            <p className="footer-quote"><em>"Cada insecto tiene su historia... ¡descúbrela!"</em></p>
+        </footer>
+    )
+
+};
+export default Footer;
